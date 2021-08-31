4496280d47c68d78f67c9b729cfc1b870e1ae148
Maksym Zyza <mmziza88@gmail.com>
2021-08-31 14:45:07 +0300

    added web server to Express - API. Error handler. Logger. Validator

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use(logger("dev"));
+app.use(cors());
+
+app.use("/weather", require("./routes/api/weather"));
+
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
---
+++ routes/api/weather.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const got = require("got");
+const router = express.Router();
+const { validationResult, query } = require("express-validator");
+
+require("dotenv").config();
+
+// middleware
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+// validator
+const validator = (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({ error: error.array() });
+  }
+  next();
+};
+
+// define the home page route
+router.get(
+  "/",
+  [query("lat").isNumeric(), query("lon").isNumeric()],
+  validator,
+  async (req, res, next) => {
+    const { lat, lon } = req.query;
+    const apiKey = process.env.API_KEY_OW;
+    try {
+      const response = await got(
+        "https://api.openweathermap.org/data/2.5/weather",
+        { searchParams: { lat, lon, appid: apiKey } }
+      );
+      return res.json(JSON.parse(response.body));
+      // const {
+      //   name,
+      //   main: { temp_max: tMax, temp_min: tMin },
+      //   wind,
+      //   weather: [weather],
+      //   sys: { country },
+      // } = JSON.parse(response.body);
+      // res.json({ name, tMax, tMin, wind, weather, country });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get("/error", function (req, res) {
+  throw new Error("Error!");
+  res.json({ message: "Error" });
+});
+
+module.exports = router;
+
+// http://localhost:3000/weather?lat=49,8383&lon=24,0232
